23a4ad9a0371c2d43b636850e1c6ffeeae7444b7
prabhasp <prabhas.pokharel@gmail.com>
2015-04-24 15:24:47 +0545

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject prabhasp/osmtogeojson-cljs "2.2.5"
+ :description "osmtogeojson"
+ :url "https://github.com/tyrasd/osmtogeojson"
+ :license {:name "Eclipse Public License"
+           :url "http://www.eclipse.org/legal/epl-v10.html"})
+
