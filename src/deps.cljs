ab56609329f587d91be732ae5f3f1beeb8ba0546
prabhasp <prabhas.pokharel@gmail.com>
2015-04-24 15:26:22 +0545

    Adding osmtogeojson + deps.cljs, v2.2.5

---
+++ src/deps.cljs
@@ -0,0 +1,4 @@
+{:foreign-libs [{:file "osmtogeojson/osmtogeojson"
+                 :file-min "osmtogeojson/osmtogeojson.js"
+                 :provides ["osmtogeojson"]}
+ :externs ["osmtogeojson-externs.js"]}
---
+++ src/osmtogeojson-externs.js
@@ -0,0 +1,1 @@
+var osmtogeojson = function();
